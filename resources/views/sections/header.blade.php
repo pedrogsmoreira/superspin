19e95f59b47c1837bb5a148988fc6cbf5e305469
Pedro Guilherme Moreira <pedrogsmoreira@gmail.com>
2025-01-22 17:02:53 +0000

    Create Vanilla App file structure

---
+++ resources/views/sections/header.blade.php
@@ -0,0 +1,9 @@
+<header class="sticky top-0 z-20 mt-8 w-full bg-white py-2">
+    <div class="container relative mx-auto">
+        <div id="header-logo" class="logo flex h-8 items-center md:h-12">
+            <x-icon name="super" class="h-full" />
+            <x-icon name="logo" class="icon h-[120%]" />
+            <x-icon name="spin" class="h-full" />
+        </div>
+    </div>
+</header>
